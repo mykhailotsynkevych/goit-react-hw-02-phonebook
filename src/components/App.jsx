77576ddcf84f89da881bd53ec62bddc37ea57c4e
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-30 16:44:43 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+state = {
+  contacts: [],
+  name: ''
+}
+
+  render() {
+    return (
+      <form>
+      <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+        />
+   </form> );
+  }
+}
